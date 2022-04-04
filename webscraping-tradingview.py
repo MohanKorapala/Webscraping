38583ae7f1af590a01553bd0c38ee33e39234579
Mohan Korapala <mohan_korapala1@baylor.edu>
2022-04-04 14:59:19 -0500

    class work 4/4

--- webscraping-tradingview.py
+++ webscraping-tradingview.py
@@ -19,13 +19,12 @@ req = Request(url, headers = headers)
 webpage = urlopen(req).read()
 soup = BeautifulSoup(webpage, "html.parser")
 title = soup.title
-print(title.text)
+#print(title.text)
 
 companies = soup.findAll("span", attrs={"class":"tv-screener__description"})
 
-for company in companies[:5]:
-    print(company.text.strip())
-
+#for company in companies[:5]:
+    #print(company.text.strip())
 
 
 #SOME USEFUL FUNCTIONS IN BEAUTIFULSOUP
@@ -38,4 +37,30 @@ for company in companies[:5]:
 #Text: nameList = Objfind(text="the prince")
 #Limit = find with limit of 1
 #keyword: allText = Obj.find(id="title",class="text")
+
+#tbody refers to the body of the table
+stock_table = soup.findAll('tbody')
+
+#do this in order to get teh cell value fo the first cloumn
+stock_table = stock_table[0]
+
+rows = stock_table.findAll('tr')
+
+for row in rows[:5]:
+    cols = row.findAll('td') #THIS IS TO SELECT ALL THE COLUMS IN TEH ROW   
+    name = cols[0].find('span', attrs = {'class':'tv-screener__description'})
+    name = name.text.strip()
+    price = float(cols[1].text)
+    change_text = cols[2].text
+    change_float = float(cols[2].text.strip('%'))
+
+    s_price = round(price/(1+change_float/100),2)
+
+    print(f"Company name: {name}")
+    print(f'Current Price: {price}')
+    print(f'% Change: {change_text}')
+    print(f'Starting Price:{s_price}')
+    
+
+
 
